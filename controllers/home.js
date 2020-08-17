b783e71a97d6b3891f1b71a91aa6330ea7a6f93d
gosteve <nomuojamaro@gmail.com>
2020-08-17 12:35:25 +0100

    home model

--- controllers/home.js
+++ controllers/home.js
@@ -1,48 +1,21 @@
-var sidebar = require("../helpers/sidebar");
+var sidebar = require("../helpers/sidebar"),
+  ImageModel = require("../models").Image;
 module.exports = {
   index: function (req, res) {
     var viewModel = {
-      images: [
-        {
-          uniqueId: 1,
-          title: "Sample Image 1",
-          description: "this is image 1",
-          filename: "sample1.jpg",
-          views: 10,
-          likes: 50,
-          timestamp: Date.now,
-        },
-        {
-          uniqueId: 2,
-          title: "Sample Image 2",
-          description: "this is image 3",
-          filename: "sample2.jpg",
-          views: 40,
-          likes: 5,
-          timestamp: Date.now,
-        },
-        {
-          uniqueId: 3,
-          title: "Sample Image 3",
-          description: "image 3",
-          filename: "sample3.jpg",
-          views: 6,
-          likes: 40,
-          timestamp: Date.now,
-        },
-        {
-          uniqueId: 4,
-          title: "Sample Image 4",
-          description: "inage 5",
-          filename: "sample4.jpg",
-          views: 40,
-          likes: 50,
-          timestamp: Date.now,
-        },
-      ],
+      images: [],
     };
-    sidebar(viewModel, function (viewModel) {
-      res.render("index", viewModel);
+    ImageModel.find({}, {}, { sort: { timestamp: -1 } }, function (
+      err,
+      images
+    ) {
+      if (err) {
+        throw err;
+      }
+      viewModel.images = images;
+      sidebar(viewModel, function (viewModel) {
+        res.render("index", viewModel);
+      });
     });
   },
 };
